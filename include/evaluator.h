c2896710ae4342ae923735e7cf4381aa2bb5b3cc
Sajjad-dev <sajjadzaidi0989@gmail.com>
2025-09-10 23:58:38 +0500

    add implementation of methods

--- include/evaluator.h
+++ include/evaluator.h
@@ -1,16 +1,19 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
-class Evaluator {
-    public:
-        Evaluator(int varCount, vector<string> expression);
-        void truthValuesGenerator(int r, int s,  vector<vector <bool>> &vals);
-    private:
-     int rows, cols;
-     vector<vector<bool>> truthVals;
-     vector<string> exp;
+class Evaluator
+{
+public:
+    Evaluator(int varCount, vector<string> expression);
+    void truthValuesGenerator();
+    void printTruthTable();
+
+private:
+    int rows, cols;
+    vector <vector<bool> > truthVals;
+    vector<string> exp;
 };
--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -1,6 +1,56 @@
 #include "../include/evaluator.h"
 
-Evaluator :: Evaluator(int varCount, vector<string> expression){
+Evaluator ::Evaluator(int varCount, vector<string> expression)
+{
     cols = varCount;
-    rows = pow(2,varCount);
+    rows = static_cast<int>(pow(2, varCount));
+    exp = expression;
 }
+
+void Evaluator ::truthValuesGenerator()
+{
+    int start;
+    int limit;
+    for (int i = 1; i <= cols; i++)
+    {
+        int row = i;
+        limit = (pow(2,i)/2)-1;
+        start = 0;
+        vector<bool> varVals;
+        for (int j = 0; j < rows; j++)
+        {
+            if (j <= limit && j >= start)
+            {
+                // cout << "false" << endl;
+                varVals.push_back(false);
+            }
+            else
+            {
+                // cout << "true" << endl;
+                varVals.push_back(true);
+                if (j > limit)
+                {
+                    limit += static_cast<int>(pow(2, row));
+                    start += static_cast<int>(pow(2, row));
+                }
+            }
+        }
+        truthVals.push_back(varVals);
+    }
+}
+
+void Evaluator ::printTruthTable()
+{
+    truthValuesGenerator();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << truthVals[j][i] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+
