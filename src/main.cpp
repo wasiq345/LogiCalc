4074cbfa709786047ab8c963e8b978c9b55f2881
wasiq345 <wasiqazeem000@gmail.com>
2025-09-13 01:31:43 +0500

    added menu interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include "../include/parser.h"
+#include "../include/tokenizer.h"
+#include "../include/evaluator.h"
+#include <iostream>
 
 int main()
 {
@@ -11,12 +14,25 @@ int main()
 
         std :: cout << "\n\n\t\t Write Expression: " << std :: endl;
         std :: cout << "\t\t Write quit to Exit" << std :: endl;
-        std :: cin >> expression;
+        std :: getline(std :: cin, expression);
         
-        // Tokenize the expression
-        // Convert infix to postfix
-        // Evaluate the postfix expression
-        // Display the result
+        if(expression == "quit") break;
+        Tokenizer tokenizer(expression);
+        auto tokens = tokenizer.tokenize();
+        Parser parser(tokens);
+        int varCount = parser.getVariableCount();
+        auto PostFixexpression = parser.parse();
+        Evaluator evaluator(varCount, PostFixexpression);
+        auto truthTable = evaluator.getTruthTable();
+        std :: cout << "\n\n\t\t Truth Table \n";
+        for(const auto& var : evaluator.variables) std :: cout << var << "\t";
+        std :: cout << "Result\n";
+        for(const auto& row : truthTable)
+        {
+            for(const auto& val : row) std :: cout << val << "\t";
+            std :: cout << "\n";
+        }
+        std :: cout << "\n\n";
 
-    } while (expression != "quit");
+    } while (true);
 }
