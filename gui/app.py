c66b2a1561f18aacae1e53832f23b8b0dfc464c2
wasiq345 <wasiqazeem000@gmail.com>
2025-10-06 13:32:54 +0500

    Fixed port issue

--- gui/app.py
+++ gui/app.py
@@ -32,4 +32,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
